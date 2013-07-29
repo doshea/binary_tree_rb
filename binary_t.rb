32cc8f5f920c62352362e77762463eb654b0c2e6
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-29 18:01:18 -0400

    Adding search capability and to_s output

--- binary_t.rb
+++ binary_t.rb
@@ -30,7 +30,44 @@ class BinaryTree
 
 
   def search(value)
+    if value < @value
+      return @left_branch.search(value) if @left_branch
+    elsif value > @value
+      return @right_branch.search(value) if @right_branch
+    else
+      return self
+    end
+    # Only hit this if you get to the end of the branch without finiding
+    return nil
+  end
 
+  def to_s
+    "#{@left_branch} << #{@value} >> #{@right_branch}"
   end
 
 end
+
+my_tree = BinaryTree.new('k')
+my_tree.insert('r')
+my_tree.insert('d')
+my_tree.insert('b')
+my_tree.insert('p')
+my_tree.insert('x')
+my_tree.insert('v')
+my_tree.insert('f')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
